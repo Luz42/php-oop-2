b032807de07c9e8e43b917d4d7eeb3a873d80676
Luz42 <a.evangelista93@gmail.com>
2022-11-01 11:47:54 +0100

    Definito GameProduct.php

---
+++ models/GameProduct.php
@@ -0,0 +1,14 @@
+<?php
+
+    require_once __DIR__ . '/Product.php';
+
+    class GameProduct extends Product{
+
+        public $category = 'Game';
+
+        public $materials = 'plastic';
+
+    }
+
+
+?>
