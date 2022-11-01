9d5fefb6149456cc23aa217305586adf532f3e6d
Luz42 <a.evangelista93@gmail.com>
2022-11-01 12:17:02 +0100

    Riscritta la funzione checkOut in RegisteredUser
    viene assegnato e calcolato uno sconto all'utente registrato

--- models/RegisteredUser.php
+++ models/RegisteredUser.php
@@ -8,7 +8,23 @@ class RegisteredUser extends User {
     public $surname;
     public $password;
     public $profileImage;
-    
+
+    private $discount = 20;
+
+    public function checkOut(){
+        $totalCartPrice = parent::checkOut();
+
+        return [
+            "total" => $totalCartPrice['total'] * $discount,
+            "vat" => $totalVat * $discount,
+            "finalPrice" => ($totalPrice + $totalVat) * $discount
+        ];   
+    }
+
+    private function getDiscount()
+    {
+        return (100 - $this->$discount) / 100;
+    }
 }
 
 ?>
